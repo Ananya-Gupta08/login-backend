f78d74e7585d38c660327ae806a663fba1ed73da
Ananya0810 <82394644+Ananya0810@users.noreply.github.com>
2026-01-23 14:00:38 +0530

    mongo url

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ console.log("SERVER FILE LOADED");
 
 // MongoDB 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/authdb")
+  .connect("process.env.MONGO_URL")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
