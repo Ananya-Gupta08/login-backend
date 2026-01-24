71cfcb79e220ed5fa935465f88d81a425f67a302
Ananya0810 <82394644+Ananya0810@users.noreply.github.com>
2026-01-24 20:28:07 +0530

    fixing mail

--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -3,12 +3,14 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to,
+  const result = await resend.emails.send({
+    from: "Login App <onboarding@resend.dev>",
+    to: [to],
     subject,
     text,
   });
+
+  console.log("RESEND RESULT:", result);
 };
 
 module.exports = sendEmail;
