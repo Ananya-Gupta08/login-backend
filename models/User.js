ca18ff3cab634c257d2fa843d9a1b5e5d4aed799
Ananya0810 <82394644+Ananya0810@users.noreply.github.com>
2026-01-30 10:46:53 +0530

    changes done

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,5 +28,4 @@ authProvider: {
   otpExpiry: Date,
 
 });
-
-module.exports = mongoose.model('User', userSchema);
+export default mongoose.model('User', userSchema);
