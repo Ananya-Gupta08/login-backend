7bfd824d4c13b2bc33a168d5d80cbceea6063556
Ananya0810 <82394644+Ananya0810@users.noreply.github.com>
2026-01-30 09:43:43 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -6,18 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-function handler(req, res) {
-  const { url } = req;
 
-  if (url === "/auth/google") {
-    return res.status(200).send("✅ /auth/google route is working");
-  }
+app.get("/", (req, res) => {
+  res.send("API working");
+});
 
-  if (url === "/auth/google/callback") {
-    return res.status(200).send("✅ Google callback route working");
-  }
-
-  return res.status(200).send("🏠 Backend root working");
-}
+app.get("/auth/google", passport.authenticate("google", {
+  scope: ["profile", "email"]
+}));
 
 export default app;
