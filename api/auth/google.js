3516f456db3a4fc54b810ec4c93e6d0d4ce6ad02
Ananya0810 <82394644+Ananya0810@users.noreply.github.com>
2026-02-04 16:16:55 +0530

    modified google.js

--- api/auth/google.js
+++ api/auth/google.js
@@ -1,15 +1,18 @@
-import { OAuth2Client } from "google-auth-library";
+// import { OAuth2Client } from "google-auth-library";
 
-export default async function handler(req, res) {
-  const client = new OAuth2Client(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET
-  );
+// export default async function handler(req, res) {
+//   const client = new OAuth2Client(
+//     process.env.GOOGLE_CLIENT_ID,
+//     process.env.GOOGLE_CLIENT_SECRET
+//   );
 
-  const url = client.generateAuthUrl({
-    scope: ["profile", "email"],
-    redirect_uri: process.env.GOOGLE_REDIRECT_URL,
-  });
+//   const url = client.generateAuthUrl({
+//     scope: ["profile", "email"],
+//     redirect_uri: process.env.GOOGLE_REDIRECT_URL,
+//   });
 
-  res.redirect(url);
+//   res.redirect(url);
+// }
+export default function handler(req, res) {
+  res.status(200).send("Google route works");
 }
